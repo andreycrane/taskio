930a1417f49a8c3fd49a470baff010d4f059b4a7
Andrey Crane <crane.andrey@gmail.com>
2014-10-08 18:45:04 +0400

    sql tables

---
+++ diy/tables.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXIST projects;
+
+CREATE TABLE IF NOT EXISTS projects (
+    id INT(11) AUTO_INCREMENT PRIMERY KEY,
+    name CHAR(255) NOT NULL,
+    color CHAR(10) NOT NULL
+);
+
+DROP TABLE IF EXIST tasks;
+
+CREATE TABLE IF NOT EXISTS tasks (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    project_id INT(11) NOT NULL,
+    done BOOL NOT NULL DEFAULT FALSE,
+    name CHAR(255) NOT NULL,
+    start_datetime DATETIME,
+    end_datetime DATETIME,
+    created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    done_at DATETIME,
+    FOREIGN KEY project_id REFERENCES projects (id)
+);
