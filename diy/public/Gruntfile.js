8ae165bdd02a176e31534e121914b05a2cd1c761
Crane Andrey <crane.andrey@gmail.com>
2014-10-12 22:37:57 +0400

    create app cache manifest file

--- diy/public/Gruntfile.js
+++ diy/public/Gruntfile.js
@@ -45,12 +45,12 @@ module.exports = function(grunt) {
                 dest: 'css/style.min.css'
             },
 
-            index: {
+            development: {
                 src: 'index.html',
                 dest: '../templates/home/index.html.ep'
             },
             
-            build: {
+            production: {
                 src: 'index_build.html',
                 dest: '../templates/home/index.html.ep'
             }
@@ -58,17 +58,18 @@ module.exports = function(grunt) {
                 
         watch: {
             grunt: {
-                files: ['Gruntfile.js']
+                files: ['Gruntfile.js'],
+                tasks: ['manifest']
             },
             
             styles: {
                 files: ['css/src/*.less'],
-                tasks: ['less:style', 'copy:styles']
+                tasks: ['less:style', 'copy:styles', 'manifest']
             },
             
             html: {
                 files: ['index.html'],
-                tasks: ['copy:index']
+                tasks: ['copy:development', 'manifest']
             }
         }
     });
@@ -79,10 +80,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     
-    grunt.registerTask('default', ['less:style']);
-    grunt.registerTask('build', ['concat:dist', 'uglify:build']);
+    grunt.registerTask('default', ['less:style', 'manifest']);
     
-    grunt.registerTask("html_build", "Build production version", function () {
+    grunt.registerTask("build:production", "Build production version", function () {
         var fileStr = grunt.file.read("index.html", { encoding: "utf8"}),
             expr,
             result,
@@ -113,12 +113,60 @@ module.exports = function(grunt) {
         }
         
         buildHtml = fileStr.replace(repl,
-            '<script src="js\\build.min.js"><\/script>');
+            '<script src="js\/build.min.js"><\/script>');
+        buildHtml = buildHtml.replace('<html>', 
+            '<html manifest="cache.manifest">');
         grunt.file.write("index_build.html", buildHtml, { encoding: "utf8" });
         
         grunt.config.set("concat_files", filesArray);
         grunt.task.run("concat:dist");
         grunt.task.run("uglify:build");
-        grunt.task.run("copy:build");
+        grunt.task.run("manifest");
+    });
+
+
+    grunt.registerTask("manifest", "Generate manifest file", function() {
+        var version = "# Task.IO cache v." + Date.now(),
+            content = [
+                "CACHE MANIFEST",
+                version,
+                // Cache
+                "CACHE:",
+                // Index
+                "index.html",
+                // Styles
+                "css/style.min.css",
+                "css/normalize.css",
+                "css/pure-nr-min.css",
+                "css/fullcalendar.css",
+                // Images
+                "img/edit.svg",
+                "img/del.svg",
+                // JavaScript
+                "js/build.min.js",
+                // Templates
+                "templates/calendar_projects.html",
+                "templates/calendar_template.html",
+                "templates/delete_project_modal.html",
+                "templates/home_template.html",
+                "templates/login_template.html",
+                "templates/project.html",
+                "templates/project_modal.html",
+                "templates/projects.html",
+                "templates/report_template.html",
+                "templates/settings_template.html",
+                "templates/task.html",
+                "templates/task_modal.html",
+                "templates/tasks.html",
+                // fonts
+                "fonts/BEBAS.ttf",
+                // NETWORK
+                "NETWORK:",
+                "/tasks",
+                "/projects"
+            ].join("\n");
+       
+        grunt.file.write('./cache.manifest', content);
+        grunt.log.ok(version); 
     });
 };
